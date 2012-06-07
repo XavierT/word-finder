aa7854f846737130e4e9b08f33eb6bade2034228
Xavier <sseingalt@gmail.com>
2012-06-07 21:14:41 +0200

    basic node constructor

--- cpp/Node.h
+++ cpp/Node.h
@@ -2,16 +2,43 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
-#include <vector>
+#include <map>
+#include <string>
 
 class Node
 {
-    char character;
-    bool is_a_word;
-    std::vector<Node *> successor;
 
-    Node();
-    ~Node();
+    public:
+    Node() :  is_a_word_(false), edges_() {} 
+
+    Node(const Node &OriginalNode)
+    {
+        // check for self assignement ?
+        std::map<char, Node *>::const_iterator i;
+
+        is_a_word_ = OriginalNode.is_a_word_;
+
+        for(i = OriginalNode.edges_.begin(); i != OriginalNode.edges_.end(); ++i)
+        {
+            edges_.insert(std::make_pair((*i).first, new Node(*(*i).second)));
+        }
+
+    }
+
+    ~Node()
+    {
+
+    }
+
+    void addEdge(char value);
+    void removeEdge(char value);
+
+    bool is_a_word_termination() { return is_a_word_; }
+
+    private:
+    bool is_a_word_;
+    std::map<char,Node *> edges_; ///< edges to other nodes, key is the edge value.
+
 
 };
 
